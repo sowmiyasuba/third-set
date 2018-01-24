8a4013e702e280e4d40961904c6b4e073bdfddbf
sowmiyasuba <31585889+sowmiyasuba@users.noreply.github.com>
2018-01-24 16:44:00 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int j,a[j],i;
+printf("enter the num:");
+scanf("%d",&j);
+for(i=0;i<j;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<j;i++){
+printf("%d %d\n",a[i],i);
+    
+}
+return 0;
+}
